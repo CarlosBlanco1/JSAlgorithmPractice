a638ffa3c7fbb43611b71dc0dcea836c16788c2a
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2026-08-11 11:36:35 -0500

    missing num problem

---
+++ search/missingnum.js
@@ -0,0 +1,14 @@
+var missingNumber = function(nums) {
+
+    nums = nums.sort((a,b) => a - b);
+
+    for(let i = 0; i <= nums.length; i++)
+    {
+        if(nums[i] != i)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+};
