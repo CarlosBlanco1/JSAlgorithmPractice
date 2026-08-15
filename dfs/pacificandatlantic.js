37c6169c8de215299d1f7fb8652ca44f1c7019f8
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2026-08-15 07:42:23 -0500

    D GOAT

--- dfs/pacificandatlantic.js
+++ dfs/pacificandatlantic.js
@@ -4,129 +4,123 @@
  */
 var pacificAtlantic = function (heights) {
 
-    var input = JSON.stringify(heights);
-    var cheat = JSON.stringify([[1,2,3],[8,9,4],[7,6,5]]);
+    var rows = heights.length;
+    var cols = heights[0].length;
 
-    if(input == cheat) return [[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
+    var pacificMatrix =  Array.from({ length: rows }, () => Array(cols).fill('N'));
+    var atlanticMatrix =  Array.from({ length: rows }, () => Array(cols).fill('N'));
 
-    var pacificReachable = new Map();
-    var atlanticReachable = new Map();
-    var rowSize = heights.length;
-    var colSize = heights[0].length;
-
-    for (let i = 0; i < heights.length; i++) {
-        for (let j = 0; j < heights[0].length; j++) {
-            const currCoords = JSON.stringify([i, j]);
-
-            if ((i == 0) || (j == 0)) {
-                pacificReachable.set(currCoords, 1);
-                if ((i == (rowSize - 1)) || (j == (colSize - 1))) {
-                    atlanticReachable.set(currCoords, 1);
-                }
-                continue;
+    for(let i = 0; i < rows; i++)
+    {
+        for(let j = 0; j < cols; j++)
+        {
+            if(i == 0 || j == 0)
+            {
+                pacificTraverser(heights, pacificMatrix, i, j);
             }
-            if ((i == (rowSize - 1)) || (j == (colSize - 1))) {
-                atlanticReachable.set(currCoords, 1);
-                continue;
+            if(j == cols - 1 || i == rows - 1)
+            {
+                atlanticTraverser(heights, atlanticMatrix, i, j);
             }
-        }
-    }
-
-    for (let i = 0; i < heights.length; i++) {
-        for (let j = 0; j < heights[0].length; j++) {
-            const currCoords = JSON.stringify([i, j]);
-
-            var currCell = heights[i][j];
-
-            try {
-                var upperCell = heights[i - 1][j];
-                var upperCoords = JSON.stringify([i - 1, j]);
-                if (pacificReachable.has(upperCoords) && (currCell >= upperCell)) {
-                    pacificReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-
-            try {
-                var leftCell = heights[i][j - 1];
-                var leftCoords = JSON.stringify([i, j - 1]);
-                if (pacificReachable.has(leftCoords) && (currCell >= leftCell)) {
-                    pacificReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-
-            try {
-                var downCell = heights[i + 1][j];
-                var downCoords = JSON.stringify([i + 1, j]);
-                if (atlanticReachable.has(downCoords) && (currCell >= downCell)) {
-                    atlanticReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-
-            try {
-                var rightCell = heights[i][j + 1];
-                var rightCoords = JSON.stringify([i, j + 1]);
-                if (atlanticReachable.has(rightCoords) && (currCell >= rightCell)) {
-                    atlanticReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-        }
-    }
-
-    for (let i = heights.length - 1; i >= 0; i--) {
-        for (let j = heights[0].length - 1; j >= 0; j--) {
-            const currCoords = JSON.stringify([i, j]);
-
-            var currCell = heights[i][j];
-
-            try {
-                var upperCell = heights[i - 1][j];
-                var upperCoords = JSON.stringify([i - 1, j]);
-                if (pacificReachable.has(upperCoords) && (currCell >= upperCell)) {
-                    pacificReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-
-            try {
-                var leftCell = heights[i][j - 1];
-                var leftCoords = JSON.stringify([i, j - 1]);
-                if (pacificReachable.has(leftCoords) && (currCell >= leftCell)) {
-                    pacificReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-
-            try {
-                var downCell = heights[i + 1][j];
-                var downCoords = JSON.stringify([i + 1, j]);
-                if (atlanticReachable.has(downCoords) && (currCell >= downCell)) {
-                    atlanticReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
-
-            try {
-                var rightCell = heights[i][j + 1];
-                var rightCoords = JSON.stringify([i, j + 1]);
-                if (atlanticReachable.has(rightCoords) && (currCell >= rightCell)) {
-                    atlanticReachable.set(currCoords, 1);
-                }
-            }
-            catch (error) { }
         }
     }
 
     results = [];
 
-    for (const [key, value] of pacificReachable) {
-        if (atlanticReachable.has(key)) {
-            results.push(JSON.parse(key));
+    for(let i = 0; i < rows; i++)
+    {
+        for(let j = 0; j < cols; j++)
+        {
+            if(pacificMatrix[i][j] == 'R' && atlanticMatrix[i][j] == 'R')
+            {
+                results.push([i, j]);
+            }
         }
     }
 
     return results;
 };
+
+function pacificTraverser(heights, pacificMatrix, r, c) {
+
+    pacificMatrix[r][c] = 'R';
+
+    var currVal = heights[r][c];
+
+    var maxRow = heights.length - 1;
+    var maxCol = heights[0].length - 1;
+
+    if ((c - 1) >= 0) {
+        var leftCellVal = heights[r][c - 1];
+
+        if (leftCellVal >= currVal && pacificMatrix[r][c -1] == 'N') {
+            pacificTraverser(heights, pacificMatrix, r, c - 1);
+        }
+    }
+
+    if ((r - 1) >= 0) {
+        var upperCellVal = heights[r - 1][c];
+
+        if (upperCellVal >= currVal && pacificMatrix[r - 1][c] == 'N') {
+            pacificTraverser(heights, pacificMatrix, r - 1, c);
+        }
+    }
+
+    if ((c + 1) <= maxCol) {
+        var rightCellVal = heights[r][c + 1];
+
+        if (rightCellVal >= currVal && pacificMatrix[r][c + 1] == 'N') {
+            pacificTraverser(heights, pacificMatrix, r, c + 1);
+        }
+    }
+
+    if ((r + 1) <= maxRow) {
+        var downCellVal = heights[r + 1][c];
+
+        if (downCellVal >= currVal && pacificMatrix[r + 1][c] == 'N') {
+            pacificTraverser(heights, pacificMatrix, r + 1, c);
+        }
+    }
+}
+
+function atlanticTraverser(heights, atlanticMatrix, r, c) {
+
+    atlanticMatrix[r][c] = 'R';
+
+    var currVal = heights[r][c];
+
+    var maxRow = heights.length - 1;
+    var maxCol = heights[0].length - 1;
+
+    if ((c - 1) >= 0) {
+        var leftCellVal = heights[r][c - 1];
+
+        if (leftCellVal >= currVal && atlanticMatrix[r][c -1] == 'N') {
+            atlanticTraverser(heights, atlanticMatrix, r, c - 1);
+        }
+    }
+
+    if ((r - 1) >= 0) {
+        var upperCellVal = heights[r - 1][c];
+
+        if (upperCellVal >= currVal && atlanticMatrix[r-1][c] == 'N') {
+            atlanticTraverser(heights, atlanticMatrix, r - 1, c);
+        }
+    }
+
+    if ((c + 1) <= maxCol) {
+        var rightCellVal = heights[r][c + 1];
+
+        if (rightCellVal >= currVal && atlanticMatrix[r][c + 1] == 'N') {
+            atlanticTraverser(heights, atlanticMatrix, r, c + 1);
+        }
+    }
+
+    if ((r + 1) <= maxRow) {
+        var downCellVal = heights[r + 1][c];
+
+        if (downCellVal >= currVal && atlanticMatrix[r + 1][c] == 'N') {
+            atlanticTraverser(heights, atlanticMatrix, r + 1, c);
+        }
+    }
+}
